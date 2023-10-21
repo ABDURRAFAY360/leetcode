8fb7425d6011f1248c0f3a4750d48c941fe8a476
Abdur Rafay <abdurrafay360@gmail.com>
2023-10-22 02:26:35 +0500

    3 sum solve

---
+++ 3sum.py
@@ -0,0 +1,53 @@
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Explanation: 
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+# Notice that the order of the output and the order of the triplets does not matter.
+# Example 2:
+
+# Input: nums = [0,1,1]
+# Output: []
+# Explanation: The only possible triplet does not sum up to 0.
+# Example 3:
+
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+# Explanation: The only possible triplet sums up to 0.
+ 
+
+# Constraints:
+
+# 3 <= nums.length <= 3000
+# -105 <= nums[i] <= 105
+
+# https://leetcode.com/problems/3sum/description/
+
+#Answer
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        for i in range(0,len(nums)):
+            x = nums[i]
+            for j in range(i+1,len(nums)):
+                y = nums[j]+x
+                z=-1*y                
+                if(z in nums):
+                    track = nums.index(z)
+                    a =  [x,nums[j],z]
+                    a.sort()
+                    if(track!=i and track!=j and a not in result):
+                        result.append(a)
+        
+        return result
